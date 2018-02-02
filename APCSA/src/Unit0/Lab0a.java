870b4376dd8341bdfd9284a36162389c5fc70b8f
zhua0120 <zhua0120@CA-SU-F106-20.sduhsd.lan>
2018-02-02 13:57:11 -0800

    Upload APCSA Project

---
+++ APCSA/src/Unit0/Lab0a.java
@@ -0,0 +1,25 @@
+package Unit0;
+
+public class  Lab0a
+{
+	public static void main ( String[] args )
+	{
+		System.out.println("Andy Zhu \t  2/1/18 \t P. 2\n\n" );
+		System.out.println("This animal is trivial and left as an exercise to the reader." );
+		System.out.println("\n\n\n\n" );
+
+
+		System.out.println("         ||||||||||             " );
+		System.out.println("         __________                " );
+		System.out.println("         |        |             " );
+		System.out.println("         |  o  o  |              " );
+		System.out.println("         |[------]|            " );
+		System.out.println("         |________|                " );
+		//add other output
+
+		System.out.println(" \n\n\n\nHelpFul Hints" );
+		System.out.println("\\\\ draws one backslash on the screen!\n" );
+		System.out.println("\\\" draws one double quote on the screen!\n" );
+		System.out.println("\\\' draws one single quote on the screen!\n" );
+	}
+}
---
+++ APCSA/src/Unit0/Lab0b.java
@@ -0,0 +1,44 @@
+package Unit0;
+
+public class Lab0b
+{
+	public static void main ( String[] args )
+	{
+		
+		byte byteOne = 127;
+		short shortone = -32123;
+		int intone = 90877;
+		long longone = 999999999;
+		float floatone = 32.5678f;
+		double doubleone = 923.234;
+		boolean booleanone = true;
+		char charone = 'a';
+		String stringone = "Hello World!";
+		
+		System.out.println("/////////////////////////////////");
+		System.out.println("*Jim Bob                08/18/06*");
+		System.out.println("*                               *");
+		System.out.println("*        integer types          *");
+		System.out.println("*                               *");
+		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+		System.out.println("*16 bit - ShortOne = "+shortone+"     *");
+		System.out.println("*32 bit - intOne = "+intone+"\t"+ "*");
+		System.out.println("*64 bit - longOne = "+longone+"   "+ "*");
+		System.out.println("*                               *");
+		System.out.println("*          real types           *");
+		System.out.println("*                               *");
+		System.out.println("*32 bit - floatOne = "+floatone+"    *");
+		System.out.println("*64 bit - doubleOne = "+doubleone+"   "+ "*");
+		System.out.println("*                               *");
+		System.out.println("*     other integer types       *");
+		System.out.println("*                               *");
+		System.out.println("*16 bit - charOne = "+charone+"           *");
+		System.out.println("*                               *");
+		System.out.println("*         other types           *");
+		System.out.println("*                               *");
+		System.out.println("* booleanOne = "+booleanone+"             *");
+		System.out.println("* stringOne = "+stringone+"      *");
+
+
+	}
+}
